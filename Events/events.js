e24851463eacd93b8b84e556ee43d42a64994612
Priyanka <73979941+priyanka467@users.noreply.github.com>
2024-10-12 18:52:22 +0000

    change

--- Events/events.js
+++ Events/events.js
@@ -2,11 +2,11 @@
    let btn = document.getElementById('btn');
    let para = document.getElementById('para');
    para.addEventListener('mouseover', function run(){
-      console.log("mouse inside")
-   })
-   para.addEventListener('mouuseout', function run(){
-      console.log("mouse went outside")
-   })
+       alert('Mouse Inside')
+   });
+   para.addEventListener('mouseout', function run(){
+      alert('Mouse now went outside')
+  });
    function toggleHide() {
    if (para.style.display !== 'none') {
        para.style.display = 'none';
